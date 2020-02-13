dd0542d992f664a8aba581bc64038ca3d7e33bfe
Alec Smecher <alec@smecher.bc.ca>
2020-02-12 16:55:08 -0800

    Update copyright dates; move to GPLv3

--- HypothesisPlugin.inc.php
+++ HypothesisPlugin.inc.php
@@ -1,15 +1,13 @@
 <?php
 
 /**
- * @file plugins/generic/hypothesis/HypothesisPlugin.inc.php
+ * @file HypothesisPlugin.inc.php
  *
- * Copyright (c) 2013-2019 Simon Fraser University
- * Copyright (c) 2003-2019 John Willinsky
- * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ * Copyright (c) 2013-2020 Simon Fraser University
+ * Copyright (c) 2003-2020 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @class HypothesisPlugin
- * @ingroup plugins_generic_hypothesis
- *
  * @brief Hypothesis annotation/discussion integration
  */
 
--- index.php
+++ index.php
@@ -1,22 +1,16 @@
 <?php
 
 /**
- * @defgroup plugins_generic_hypothesis
- */
- 
-/**
- * @file plugins/generic/hypothesis/index.php
+ * @file index.php
  *
- * Copyright (c) 2013-2019 Simon Fraser University
- * Copyright (c) 2003-2019 John Willinsky
- * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ * Copyright (c) 2013-2020 Simon Fraser University
+ * Copyright (c) 2003-2020 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
- * @ingroup plugins_generic_hypothesis
  * @brief Wrapper for Hypothes.is plugin.
  *
  */
 
 require_once('HypothesisPlugin.inc.php');
-
 return new HypothesisPlugin();
 
