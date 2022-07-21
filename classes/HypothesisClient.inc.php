4270fcac96f3d49f7dce7b34c28f4d85ab08b841
Jhon <jhon@lepidus.com.br>
2022-07-21 14:59:27 -0400

    Finishes HypothesisClient class
    It can now request the number of annotations of a galley to the Hypothesis API
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>
    Signed-off-by: Vitoria <vitoria@lepidus.com.br>

--- classes/HypothesisClient.inc.php
+++ classes/HypothesisClient.inc.php
@@ -2,7 +2,7 @@
 
 class HypothesisClient {
 
-    public function getGalleyAnnotationsNumber($galley) {
+    private function getGalleyDownloadURL($galley) {
         $request = Application::get()->getRequest();
         $indexUrl = $request->getIndexUrl();
         $contextPath = $request->getContext()->getPath();
@@ -12,8 +12,30 @@ class HypothesisClient {
         $assocId = $submissionFile->getData('assocId');
         $submissionFileId = $submissionFile->getId();
         
-        $galleyLinkUrl = $indexUrl . "/$contextPath/preprint/download/$submissionId/$assocId/$submissionFileId";
-        return 10;
+        return $indexUrl . "/$contextPath/preprint/download/$submissionId/$assocId/$submissionFileId";
+    }
+
+    private function checkRequestStatus($responseHeaders) {
+        $partitionedStatus = explode(" ", $responseHeaders[0]);
+        
+        $responseStatusCode = $partitionedStatus[1];
+        $httpOKCode = "200";
+
+        return $responseStatusCode == $httpOKCode;
+    }
+
+    public function getGalleyAnnotationsNumber($galley) {
+        $galleyFileDownloadURL = $this->getGalleyDownloadURL($galley);
+        $requestURL = "https://hypothes.is/api/search?limit=0&group=__world__&uri={$galleyFileDownloadURL}";
+
+        $requestSuccedeed = $this->checkRequestStatus(get_headers($requestURL));
+        if(!$requestSuccedeed)
+            return null;
+        
+        $response = json_decode(file_get_contents($requestURL), true);
+        $annotationsNumber = $response['total'];
+
+        return $annotationsNumber;
     }
 
 }
