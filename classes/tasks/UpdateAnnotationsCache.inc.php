dcfd8ae12bb86b78db61f312898f2d7003cec1d8
Jhon <jhon@lepidus.com.br>
2024-11-07 17:17:13 -0400

    Fixes error in task for updating annotations cache
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- classes/tasks/UpdateAnnotationsCache.inc.php
+++ classes/tasks/UpdateAnnotationsCache.inc.php
@@ -1,33 +1,34 @@
 <?php
 
 import('lib.pkp.classes.scheduledTask.ScheduledTask');
+import('plugins.generic.hypothesis.classes.HypothesisHelper');
 
-class UpdateAnnotationsCache extends ScheduledTask {
-
-	public function executeActions() {
-        $contextIds = Services::get('context')->getIds([
+class UpdateAnnotationsCache extends ScheduledTask
+{
+	public function executeActions()
+	{
+		$contextIds = Services::get('context')->getIds([
 			'isEnabled' => true,
 		]);
-        
-        foreach ($contextIds as $contextId) {
-            $cacheManager = CacheManager::getManager();
-            $cache = $cacheManager->getFileCache(
-                $contextId,
-                'submissions_annotations',
-                [$this, 'cacheDismiss']
-            );
 
-            $cache->flush();
-            $hypothesisHandler = new HypothesisHandler();
-			$cache->setEntireCache($hypothesisHandler->getSubmissionsAnnotations($contextId));
-        }
+		foreach ($contextIds as $contextId) {
+			$cacheManager = CacheManager::getManager();
+			$cache = $cacheManager->getFileCache(
+				$contextId,
+				'submissions_annotations',
+				[$this, 'cacheDismiss']
+			);
+
+			$cache->flush();
+			$hypothesisHelper = new HypothesisHelper();
+			$cache->setEntireCache($hypothesisHelper->getSubmissionsAnnotations($contextId));
+		}
 
 		return true;
 	}
 
-    function cacheDismiss() {
+	public function cacheDismiss()
+	{
 		return null;
 	}
 }
-
-
